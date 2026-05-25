257fde75c63c2ec575065ebb253d20635cdd794c
Greyclaw <greyclawbox@gmail.com>
2026-05-25 16:13:26 +0300

    Ball Scene

---
+++ scenes/ball.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var base_speed = 400
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	velocity = Vector2(-base_speed, base_speed / 2)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+		
